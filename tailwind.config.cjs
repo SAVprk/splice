0890ce3a4fc8d819cbe1da65c6acd74d56d80d00
SAVprk <90642191+SAVprk@users.noreply.github.com>
2023-03-01 16:30:03 +0300

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./pages/**/*.{html,js}", "./index.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          Oswald: ["oswald", "serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
